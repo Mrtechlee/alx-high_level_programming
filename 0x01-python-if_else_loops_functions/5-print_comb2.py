28cdadacdcee45cf016fd153b6fe8cca0f8ca028
EDWARD GRITCODE LEON KHOURY <107373151+Mrtechlee@users.noreply.github.com>
2023-02-08 03:11:43 +0000

    correction

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for num in range(00, 100):
-    print("{:02d}".format(num), end='\n' if num == 99 else ", ")
+for i in range(100):
+    if i == 99:
+        print(i)
+    else:
+        print("{}".format('0' + str(i) if i < 10 else i), end=", ")
