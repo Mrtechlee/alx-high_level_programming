5744a30bf25c2226942960c72f4003706f86e526
EDWARD GRITCODE LEON KHOURY <107373151+Mrtechlee@users.noreply.github.com>
2023-02-13 12:58:22 +0000

    update

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,2 +1,4 @@
--- Lists all the tables of my database in my MySQL server
+-- lists all the tables of a database in your MySQL server.
+-- The database name will be passed as argument of mysql command
+
 SHOW TABLES;
